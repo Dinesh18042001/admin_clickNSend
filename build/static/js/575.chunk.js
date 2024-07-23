a540d6386ebcc2a36578a2cfa5295c782000f70f
Dinesh18042001 <dinesh@1804200182@gmail.com>
2024-07-23 10:28:36 +0530

    update

---
+++ build/static/js/575.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklogistic_app=self.webpackChunklogistic_app||[]).push([[575],{575:(s,p,c)=>{c.r(p),c.d(p,{default:()=>e});const e=c(22849).z}}]);
+//# sourceMappingURL=575.chunk.js.map
